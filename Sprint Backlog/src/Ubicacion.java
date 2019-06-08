1435c4c17537afb082600f3f6265a3a45df1edf4
GonzaSuarez <legonza424@gmail.com>
2019-06-08 17:00:25 -0300

    Ubicacion Class

---
+++ Sprint Backlog/src/Ubicacion.java
@@ -0,0 +1,19 @@
+
+public class Ubicacion {
+	
+	private float x;
+	private float y;
+	
+	public Ubicacion(float a, float b) {
+		x=a;
+		y=b;
+	}
+	
+	public float getX() {
+		return x;
+	}
+	
+	public float getY() {
+		return y;
+	}
+}

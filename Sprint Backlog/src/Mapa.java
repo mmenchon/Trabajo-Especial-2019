d4fd34b098be0d1847675021f51cf7db5a7d9c42
GonzaSuarez <legonza424@gmail.com>
2019-06-08 18:04:28 -0300

    Class Mapa

---
+++ Sprint Backlog/src/Mapa.java
@@ -0,0 +1,17 @@
+import java.util.Vector;
+
+public class Mapa {
+	
+	private Vector<Ubicacion> map;
+	private Vector<PuntoVerde> puntos;
+	
+	public Mapa(Ubicacion m, PuntoVerde pv) {
+		map.add(m);
+		puntos.add(pv);
+	}
+	
+	public Mapa getMapa() {
+		return this;
+	}
+	
+}

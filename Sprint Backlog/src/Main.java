efd9dc9668e7f6040cbac16f3d3fb3ba01a5042a
leonell147 <49492744+leonell147@users.noreply.github.com>
2019-06-10 16:47:56 -0300

    vecinos en main

--- Sprint Backlog/src/Main.java
+++ Sprint Backlog/src/Main.java
@@ -4,5 +4,12 @@ public class Main {
         System.out.println("Hello World!");
     }
 
-    Vecino v1 = new Vecino("skrr","capo", "")
+    Vecino v1 = new Vecino("Juan","juani", "pepo1");
+    Vecino v2 = new Vecino("Carlos","carlitos", "pepo2");
+    Vecino v3 = new Vecino("Pedro","pedrito", "pepo3");
+    Vecino v4 = new Vecino("Mauri","maurito", "pepo4");
+    Vecino v5 = new Vecino("Mariano","nanito", "pepo5");
+    Vecino v6 = new Vecino("Leo","leito", "pepo6");
+
+
 }

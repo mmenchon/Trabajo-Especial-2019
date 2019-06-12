c2ced3fd530a61fe2d1e2a6115f38d0179bb09a4
GonzaSuarez <legonza424@gmail.com>
2019-06-12 14:44:25 -0300

    Update OrdenRanking

--- Sprint Backlog/src/Entidades/OrdenRanking.java
+++ Sprint Backlog/src/Entidades/OrdenRanking.java
@@ -8,13 +8,19 @@ public class OrdenRanking implements Comparator<Vecino> {
 
 	@Override
 	public int compare(Vecino o1, Vecino o2) {
-		if(o1.getPuntaje() <= o2.getPuntaje())
-		{
-			return 0;
-		}
-		else
+		if(o1.getPuntaje() < o2.getPuntaje())
 		{
 			return 1;
 		}
-	}
+		else
+		{
+			if(o1.getPuntaje() > o2.getPuntaje())
+			{
+				return -1;
+			}
+			else {
+				return 0;
+			}
+		}
+	}	
 }
